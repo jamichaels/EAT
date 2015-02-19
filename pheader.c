c3ce9e8fffdcdb8f3f7a75e557673055fd5fa82e
jamichaels <jamichaels@gmail.com>
2015-02-19 02:49:54 -0800

    Update pheader.c

--- pheader.c
+++ pheader.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <elf.h>
 #include "eat.h"
+#include "Ptype.h"
 
 main(int argc, char **argv[]) {
 
@@ -17,10 +18,17 @@ main(int argc, char **argv[]) {
 
     FILE* fp;
 
-    if(argc < 2) { goto usage; }
+    char ptype[30];
+
+    if(argc < 2) { 
+        printf("\n64 bit ELF Program Header Reader v 0.01a - sblip - Metaphase/KNTLC\n");
+        printf("usage: %s <file>\n",(char *)argv[0]);
+        exit(-1);
+    }
     
     // Open the file and read the ELF header
     fp = fopen((char *)argv[1], "r");
+    if(fp <=0) { printf("Error reading %s \n",(char *)argv[1]); exit(-1); }
     fread(&ehdr,sizeof(Elf64_Ehdr),1,fp);
 
     // Is it a valid ELF file?
@@ -32,14 +40,18 @@ main(int argc, char **argv[]) {
 
     printf("Program Header Offset: %li\n",ehdr.e_phoff);
     printf("Number of Program Header Entries: %x\n",ehdr.e_phnum);
-    printf("Program Header Entry Size: %x\n",ehdr.e_phentsize);
+    printf("Program Header Entry Size: %x\n\n",ehdr.e_phentsize);
     
     lseek(fp,ehdr.e_phoff,0);
     for(i=0;i<ehdr.e_phnum;i++) {
 
+
         fread(&phdr,sizeof(Elf64_Phdr),1,fp);
-            printf("Entry:\n");
-            printf("Segment type: 0x%x\n",(Elf64_Word)phdr.p_type);
+    
+            getptype((long)phdr.p_type,ptype);
+
+            printf("Entry %d:\n",i);
+            printf("Segment type: %s :0x%x\n",ptype,phdr.p_type);
             printf("Segment flags: %x\n",phdr.p_flags);
             printf("Segment file offset: %li\n",phdr.p_offset);
             printf("Segment virtual address: %li\n",phdr.p_vaddr);
@@ -53,10 +65,6 @@ main(int argc, char **argv[]) {
     }
 
 
-usage:
-    printf("\n64 bit ELF Program Header Reader v 0.01a - sblip - Metaphase\n");
-    printf("usage: %s <file>\n",(char *)argv[0]);
-    exit(-1);
 }
 
 not_elf()
